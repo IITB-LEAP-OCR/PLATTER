a78e3dab23e7ca057e5dfb5478f3e47c7e724404
kasuba-badri-vishal <kasubabadri@gmail.com>
2023-11-10 09:53:41 +0530

    Added CHIPS creation data

---
+++ src/config.py
@@ -0,0 +1,19 @@
+
+
+MIN_WORD_H = 32
+MAX_WORD_H = 65    
+PAGE_W = 1024
+PAGE_H = 1024
+SPACE_X = 32
+SPACE_Y = 32
+BORDER_CUT_X = 3.5
+BORDER_CUT_Y = 7
+
+
+FOLDER_NAME = 'CHIPS_2'
+
+INPUT_FOLDER  = '/data/BADRI/DATASETS/BENCHMARK/RECOGNITON/HANDWRITTEN/IIIT_INDIC_HW_WORDS/'
+OUTPUT_FOLDER = '/data/BADRI/RECOGNITION/PAGE_LEVEL/' + FOLDER_NAME + '/'
+
+LANGUAGES = ['bengali', 'gujarati', 'gurumukhi', 'hindi', 'kannada', 'malayalam','odia', 'tamil', 'telugu', 'urdu']
+SETS = ['val', 'test', 'train']
